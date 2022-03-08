4074eb6b491350c471c7952ff868ee2bfc91378a
Pedro Cabral <pecabralsantos@gmail.com>
2022-03-08 18:51:27 -0300

    Validação simples.

--- Eventos/app/src/main/java/br/com/cabral/eventos/ui/dialog/CheckInDialogFragment.kt
+++ Eventos/app/src/main/java/br/com/cabral/eventos/ui/dialog/CheckInDialogFragment.kt
@@ -3,6 +3,8 @@ package br.com.cabral.eventos.ui.dialog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.DialogFragment
 import br.com.cabral.eventos.databinding.DialogFragmentCheckInBinding
 import br.com.cabral.eventos.ui.util.Constants.Companion.DIALOG_FRAGMENT_BUNDLE_EMAIL
@@ -18,6 +20,40 @@ class CheckInDialogFragment : DialogFragment() {
         val dialog = AlertDialog.Builder(requireContext())
         dialog.setView(binding.root)
 
+        setupValidation()
+        setupObservers()
+
+        return dialog.create()
+    }
+
+    private fun setupValidation() {
+        binding.etName.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                validateButton()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                validateButton()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+    }
+
+    private fun validateButton() {
+        binding.btnMakeReservation.isEnabled =
+            !(binding.etName.text.toString().isEmpty() || binding.etEmail.text.toString().isEmpty())
+    }
+
+    private fun setupObservers() {
+        binding.btnCancel.setOnClickListener { dismiss() }
+
         binding.btnMakeReservation.setOnClickListener {
             val name = binding.etName.text.toString()
             val email = binding.etEmail.text.toString()
@@ -29,8 +65,6 @@ class CheckInDialogFragment : DialogFragment() {
             }
             dismiss()
         }
-
-        return dialog.create()
     }
 
 }
